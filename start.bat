f65ac65f2fb9d4948c00e0802ea0604600400e2f
JEECG <445654970@qq.com>
2025-07-09 19:05:40 +0800

    敲敲云v2.1.0发布，开放用户创建和支持修改密码

---
+++ start.bat
@@ -0,0 +1,1 @@
+java -javaagent:qiaoqiaoyun-start-2.0.jar="-pwd 5eez3Vqil97n" -jar -Dfile.encoding=UTF-8 qiaoqiaoyun-start-2.0.jar
--- 安装源/config_docker/init-mongo.js
+++
@@ -1,6 +0,0 @@
-db = db.getSiblingDB('qiaoqiaoyun'); // 切换到 qiaoqiaoyun 数据库
-db.createUser({
-  user: 'admin',
-  pwd: '123456',
-  roles: [{ role: 'readWrite', db: 'qiaoqiaoyun' }]
-});
